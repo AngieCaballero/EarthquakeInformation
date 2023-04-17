822a8c2541cd15b22d2ae9d57521bd8178011df1
AngieCaballero <karma2000angiejaz@gmail.com>
2023-04-17 16:22:21 -0600

    Fix dependency and create mvvm arquitecture directory

--- app/src/main/java/com/example/earthquakeinformation/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/earthquakeinformation/ui/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.earthquakeinformation
+package com.example.earthquakeinformation.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.earthquakeinformation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
